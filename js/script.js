fa14c5a83ec9c3e1b54e87b7982ecd0c29954293
GIZA-dev <68414316+GIZA-dev@users.noreply.github.com>
2020-07-27 16:29:40 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+	$('.clients-carousel').slick({
+		arrows: false,
+		dots: false,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 2,
+		slidesToScroll: 2,
+		variableWidth: true,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+					infinite: true,
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+			// You can unslick at a given breakpoint now by adding:
+			// settings: "unslick"
+			// instead of a settings object
+		]
+	});
+	var toggleButton = $('.menu-toggle'),
+		nav = $('.main-navigation');
+
+	toggleButton.on('click', function (event) {
+		event.preventDefault();
+		toggleButton.toggleClass('is-clicked');
+		nav.toggleClass('is-clicked');
+	});
+
+});
